87bf7c7ec4e8525d7fb5f7a110caf7472ec35a4e
ted <linyc1983@gmail>
2017-01-11 17:52:44 +0800

    add some methods

--- mssql_tutorial.sql
+++ mssql_tutorial.sql
@@ -4,3 +4,6 @@ update test.dbo.Todos set title = 'new123' WHERE id = 1
 
 
 DELETE FROM test.dbo.Todos WHERE id = 3
+
+
+insert into test.dbo.about_detail (id,images,name,description) values (1,'hello.png','ted','eric')
